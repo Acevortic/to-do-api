858fe06c280594c816163384936dfb1000482e27
Michael DeCuire <michaeldecuire18@gmail.com>
2025-03-12 14:35:36 -0500

    Removed secret from commit history

--- Frontend/to-do-frontend/src/App.jsx
+++ Frontend/to-do-frontend/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
+import { response } from 'express'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([]);
+  // const axios = require('axios/dist/node/axios.cjs'); // node
+  const API_URL = "http://localhost5000/tasks";
+
+  useEffect(() => {
+    axios.get(API_URL)
+    .then(response => setTasks(response.data))
+    .catch(error => console.error("Error fetching tasks", error));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>
+        Task List: 
+      </h1>
+      <ul>
+        {tasks.map(task => (
+          <li key={task._id}>{task.title}</li>
+        ))}
+      </ul>
+
+
+    </div>
   )
 }
 
-export default App
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+export default App;
